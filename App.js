88ac41b9d242f309b52e49a2a100d0b06efd8ee0
Colm Carew <colm.carew@distilled.ie>
2017-09-14 17:27:06 +0100

    Trying to get a navdrawer - failing need to look more into this

--- App.js
+++ App.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import { View } from 'react-native';
+import { AppRegistry,  Text} from 'react-native';
 import GoogleMap from './components/maps/GoogleMap';
 
 
 export default class App extends React.Component {
+
+  componentDidMount(){
+  	navigator.geolocation.getCurrentPosition((position) => {
+  		const lat = parseFloat(position.coords.latitude);
+  		const lng = parseFloat(position.coords.longitude);
+  		const latLng = {
+  			latitude: lat,
+  			longitude: lng
+  		};
+  		console.log('Obtained LatLng');
+  		console.log(latLng);
+  	},
+  	(error) => {
+      console.log('Error getting location from navigator');
+      console.log(error);
+    },
+  	{enabledHighAccuracy: true, timeout: 2000, maximumAge: 1000});
+  }
 
   render() {
     return (
